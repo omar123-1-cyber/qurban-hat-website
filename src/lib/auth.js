3f7331bfa2f0c525a84d8e09d980137e37d9d5a0
my first github <sayeedomar359@gmail.com>
2026-04-30 21:52:46 +0800

    add routejs

---
+++ src/lib/auth.js
@@ -0,0 +1,18 @@
+import { betterAuth } from "better-auth";
+
+export const auth = betterAuth({
+  secret: process.env.BETTER_AUTH_SECRET,
+  database: {
+    provider: "sqlite",
+    url: "./db.sqlite",
+  },
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    },
+  },
+});
